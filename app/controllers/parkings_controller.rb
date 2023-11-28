5865317a96c80b92cb731bceb5eccb303e9d3b51
Anthony Girard <antho.girard18@gmail.com>
2023-11-28 17:39:00 +0100

    controllers_branch

--- app/controllers/parkings_controller.rb
+++ app/controllers/parkings_controller.rb
@@ -1,8 +1,42 @@
 class ParkingsController < ApplicationController
+  before_action :set_parking, only: [:show, :edit, :update]
+
   def index
     @parkings = Parking.all
   end
+
   def show
+    # Utilisation du before_action pour définir @parking
+  end
+
+  def new
+    @parking = Parking.new
+  end
+
+  def edit
+    # Utilisation du before_action pour définir @parking
+  end
+
+  def update
+    if @parking.update(parking_params)
+      if !@parking.occupied
+        redirect_to @parking, notice: 'Place occupée avec succès.'
+      else
+        redirect_to @parking, notice: 'Place libérée avec succès.'
+      end
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_parking
     @parking = Parking.find(params[:id])
+  end
+
+  def parking_params
+    # Ajoutez ici les paramètres autorisés lors de la mise à jour du parking
+    params.require(:parking).permit(:nom, :adresse, :capacite, :autre_attribut)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :parkings, only: [:index, :show]
 
-  get '/parkings/:id', to: 'parkings#show', as: 'parking'
+  # get '/parkings/:id', to: 'parkings#show', as: 'parking'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
