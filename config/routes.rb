5607e66092479c6a029e96a85d0ebe1e90b4a0dc
Anthony Girard <antho.girard18@gmail.com>
2023-11-30 10:10:47 +0100

    méthodes new & create

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Use resources for RESTful routes
   resources :parkings
+
+  # Use the standard new and create actions for creating a new parking
   get 'create_new_parking', to: 'parkings#new', as: 'create_new_parking'
   post 'create_parking', to: 'parkings#create', as: 'create_parking'
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
+  # Uncomment the following line if you want to define a root path route ("/")
   # root "posts#index"
 end
