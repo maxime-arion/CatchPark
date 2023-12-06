cc40efa0932343ae921d578af875d11aa9caeb83
Anthony Girard <antho.girard18@gmail.com>
2023-12-06 23:03:47 +0100

    show et edit parking

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -2,4 +2,11 @@ class Parking < ApplicationRecord
   belongs_to :user, optional: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  def status_checkbox
+    status ? '1' : '0'
+  end
+
+  def status_checkbox=(value)
+    self.status = value == '1'
+  end
 end
