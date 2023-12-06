d3b1af5c8b366c0562b28dd0da98803763e0b892
Maxime Arion <maxime.arion.pro@gmail.com>
2023-12-06 16:31:37 +0100

    front2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
     # Ajoutez également cette ligne si vous voulez permettre la mise à jour de username
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
+
+  def after_sign_in_path_for(resource)
+parkings_path
+  end
 end
