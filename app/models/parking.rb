dc39ce3409478e267ee67a80c61c717d4e7b2656
Anthony Girard <antho.girard18@gmail.com>
2023-12-08 10:12:01 +0100

    update_timer2

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -5,18 +5,18 @@ class Parking < ApplicationRecord
 
 # app/models/parking.rb
 
-before_validation :set_default_status, on: :create
+# before_validation :set_default_status, on: :create
 
-def set_default_status
-  # Utilisez le statut fourni dans les seeds s'il est présent, sinon, définissez-le sur 'true'
-  self.status = status.present? ? status : true
+# def set_default_status
+#   # Utilisez le statut fourni dans les seeds s'il est présent, sinon, définissez-le sur 'true'
+#   self.status = status.present? ? status : true
 
-  # Définissez le start_time et le end_time uniquement si le statut est 'true'
-  if self.status
-    self.start_time = Time.current
-    self.end_time = start_time + duration.hours if start_time.present? && duration.present?
-  end
-end
+#   # Définissez le start_time et le end_time uniquement si le statut est 'true'
+#   if self.status
+#     self.start_time = Time.current
+#     self.end_time = start_time + duration.hours if start_time.present? && duration.present?
+#   end
+# end
 
 
   def status_checkbox
